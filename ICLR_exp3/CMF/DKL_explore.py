39df555a4308881e7d9fd55c5811db696dde5ab4
Zhenjie Lu <1729423896@qq.com>
2025-04-21 20:10:15 +0800

    prepare for nips

--- ICLR_exp3/CMF/DKL_explore.py
+++ ICLR_exp3/CMF/DKL_explore.py
@@ -232,10 +232,10 @@ if __name__ == '__main__':
     data_name = args.data_name
     start = args.start_seed
     
-    Exp_marker = 'DKL5_explore'
+    Exp_marker = 'Norm_res'
     
     # Set up logging
-    log_file_path = os.path.join(sys.path[-1], 'ICLR_exp', 'CMF', 'Exp_results', Exp_marker, data_name, args.cost_type , 'experiment.log')
+    log_file_path = os.path.join(sys.path[-1], 'ICLR_exp3', 'CMF', 'Exp_results', Exp_marker, data_name, args.cost_type , 'experiment.log')
     log_dir = os.path.dirname(log_file_path)
 
     if not os.path.exists(log_dir):
